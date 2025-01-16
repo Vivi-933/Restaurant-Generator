8eb2b13722be30d96075ad2c778c058a4965c20c
Vivi-933 <viviolet8484@gmail.com>
2025-01-16 18:46:24 -0500

    tidying code

--- user_file.py
+++ user_file.py
@@ -13,16 +13,21 @@ CRITERIA = ['price', 'vegan', 'origin', 'type']
 
 
 def end():
+    '''
+    Gives user option to end or continue program
+    '''
     cont = input('Would you like to do anything else? (Y/N) ')
     if cont == 'Y':
         run()
     else:
         print('Have a great day!!')
         fv.close()
-    return
 
 def generate():
-    # creates list of restaurant objects
+    '''
+    creates list of restaurant objects
+    use: res_list = generate()
+    '''
     line = fv.readline()
     res_list = []
     while line != '':
@@ -33,11 +38,17 @@ def generate():
     return res_list
 
 def create_menu():
+    '''
+    lets user add a restaurant to the restaurant file
+    '''
     create_restaurant()
     end()
-    return
 
-def run():    
+
+def run():
+    '''
+    Lets user run the program
+    '''   
     print('What would you like to do?')
     for i, opt in enumerate(OPTIONS):
         print(f'{i}: {opt}')
@@ -49,35 +60,24 @@ def run():
         create_menu()
 
     # asks user to search for restaurant by name
-    elif result == 1:
-        name = input('Name: ')
-        print()
-        line = fv.readline()
-        found = False
-        for i in res_list:
-            if i.name() == name:
-                print(i)
-                break
-            if found is False:
-                add = input('''no such restaurant exists.
-Would you like to add it?(Y/N) ''')
-                if add == 'Y':
-                    create_menu()
-                else:
-                    end()
 
     elif result == 2:
         print('What criterion would you like to filter by? ')
 
         # prints options menu
         for i, crit in enumerate(CRITERIA):
-            print(f'{i+1}: {crit}')
-        # ask user what to filter by    
+            print(f'{i}: {crit}')
+
+        # ask user what to filter by
         criterion = int(input('Option: '))
-        assert criterion < 5, 'Not an option'
+        assert criterion in range(0,4)
+        # ask user what to filter by
         filt = input('Filter by: ')
         # initialize found variable
         found = False
+        for i in res_list:
+            if i == filt:
+                print(i)
 
     elif result == 3:
         print('What criterion would you like to filter by? ')
@@ -87,6 +87,23 @@ Would you like to add it?(Y/N) ''')
         assert criterion < 4, 'Not an option'
         filt = input('Criteria: ')
 
+def option1():
+    name = input('Name: ')
+    print()
+    found = False
+    # iterates through list of restaurants until a name match is found
+    for i in res_list:
+        if i.name() == name:
+            print(i)
+            found = True
+            break
+        if found is False:
+            add = input('''no such restaurant exists.
+Would you like to add it?(Y/N) ''')
+            if add == 'Y':
+                create_menu()
+            else:
+                end()
 
 res_list = generate()
 run()
